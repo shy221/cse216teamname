1d1cc60f1e8b9c797da6275c7322e166ab8f45f3
Yuming Tian <yut222@lehigh.edu>
2019-09-03 22:26:41 -0400

    set up a simple script for deploying the web front-end

---
+++ web/deploy.sh
@@ -0,0 +1,20 @@
+# deploy script for the web front-end
+
+# This file is responsible for preprocessing all TypeScript files, making sure
+# all dependencies are up-to-date, and copying all necessary files into the
+# web deploy directory.
+
+# This is the resource folder where maven expects to find our files
+TARGETFOLDER=../backend/src/main/resources
+
+# This is the folder that we used with the Spark.staticFileLocation command
+WEBFOLDERNAME=web
+
+# step 1: make sure we have someplace to put everything.  We will delete the
+#         old folder tree, and then make it from scratch
+rm -rf $TARGETFOLDER
+mkdir $TARGETFOLDER
+mkdir $TARGETFOLDER/$WEBFOLDERNAME
+
+# there are many more steps to be done.  For now, we will just copy an HTML file
+cp index.html $TARGETFOLDER/$WEBFOLDERNAME
