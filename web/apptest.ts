6380b3b8102ef05a89607bb958d0a4e3cffa2b30
lez221 <lez221@lehigh.edu>
2019-09-23 22:04:27 -0400

    Fix bug

--- web/apptest.ts
+++ web/apptest.ts
@@ -3,9 +3,14 @@ var it: any;
 var expect: any;
 var $: any;
 
+function clickEditbyRowIndex(rowIndex) { // rowIndex >=0
+    element.all(by.css('table > tbody > tr > td > button.ElementList-detailbtn'))
+   .get(rowIndex).click();
+}
+
 describe("Tests of UI: Add Post feature", function() {
 
-    it("UI Test: Add Button Show Modal", function(){
+    it("UI Test: Add Button Show Add Modal", function(){
         // click the button for showing the modal
         $('#Navbar-add').click();
         expect($("#NewEntryForm").attr("style").indexOf("display: none;")).toEqual(-1);
@@ -13,12 +18,36 @@ describe("Tests of UI: Add Post feature", function() {
         $('#NewEntryForm-Close').click();        
     });
 
-    it("UI Test: Add Button Show Modal", function(){
+    it("UI Test: Close Button Hide Modal", function(){
         // click the button for showing the modal
         $('#Navbar-add').click();
         expect($("#NewEntryForm").attr("style").indexOf("display: none;")).toEqual(-1);
         expect($("#ElementList").attr("style").indexOf("display: none;")).toEqual(-1);
+        $('#NewEntryForm-Close').click();
+        expect($("#NewEntryForm").attr("style").indexOf("display: none;")).toEqual(-1);
+        expect($("#ElementList").attr("style").indexOf("display: none;")).toEqual(-1);
+    });
+
+});
+
+describe("Tests of UI: Edit Post feature", function() {
+
+    it("UI Test: Edit Button Show Modal", function(){
+        // click the button for showing the modal
+        $('#ElementList-detailbtn').click();
+        expect($("#NewEntryForm").attr("style").indexOf("display: none;")).toEqual(-1);
+        expect($("#ElementList").attr("style").indexOf("display: none;")).toEqual(-1);
         $('#NewEntryForm-Close').click();        
+    });
+
+    it("UI Test: Close Button Hide Modal", function(){
+        // click the button for showing the modal
+        $('#Navbar-add').click();
+        expect($("#NewEntryForm").attr("style").indexOf("display: none;")).toEqual(-1);
+        expect($("#ElementList").attr("style").indexOf("display: none;")).toEqual(-1);
+        $('#NewEntryForm-Close').click();
+        expect($("#NewEntryForm").attr("style").indexOf("display: none;")).toEqual(-1);
+        expect($("#ElementList").attr("style").indexOf("display: none;")).toEqual(-1);
     });
 
 });
