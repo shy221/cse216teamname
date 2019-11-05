4a012b460992e9c3c594c39891915888df600c54
zoeyxiao <zex220@lehigh.edu>
2019-11-04 22:10:12 -0500

    modified app.ts

--- web/app.ts
+++ web/app.ts
@@ -23,6 +23,38 @@ var uemail: String;
 var ukey: String;
 var loginState: boolean = false;
 var uid: number;
+var signedin: boolean = false;
+
+var changeProfile = function(googleUser: gapi.auth2.GoogleUser) {
+  // See if `GoogleUser` object is obtained
+  // If not, the user is signed out
+  if (googleUser) {
+    // Get `BasicProfile` object
+    var profile = googleUser.getBasicProfile();
+    // Get user's basic profile information
+    /*profile = {
+      name: profile.getName(),
+      email: profile.getEmail(),
+      imageUrl: profile.getImageUrl()
+    };*/
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Email: ' + profile.getEmail()); 
+    /*toast('show-toast', {
+      text: "You're signed in."
+    });*/
+    //$.dialog.close();
+  } else {
+    // Remove profile information
+    // Polymer will take care of the rest
+    
+    //profile = null;
+    
+    /*fire('show-toast', {
+      text: "You're signed out."
+    });*/
+  }
+};
 
 // Run some configuration code when the web page loads
 $(document).ready(function () {
@@ -35,20 +67,60 @@ $(document).ready(function () {
     UserProfile.refresh();
     ShowComments.refresh();
     EditUserProfile.refresh();
-    /*gapi.load('auth2', function() {
+    var google : string = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https%3A%2F%2Farcane-refuge-67249.herokuapp.com&response_type=code&client_id=689219964832-6m703l22ir6jh9ra1m1lhrgg12bv7olt.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&include_granted_scopes=true";
+    
+    var home : string = "https://arcane-refuge-67249.herokuapp.com/"
+    gapi.load('auth2', function() {
         // Initialize `auth2`
-        gapi.auth2.init().then(function(auth2) {
-            
+        gapi.auth2.init({
+          client_id: '689219964832-6m703l22ir6jh9ra1m1lhrgg12bv7olt.apps.googleusercontent.com'
+        }).then(function(auth2) {
           // If the user is already signed in
           if (auth2.isSignedIn.get()) {
+            console.log('signed in');
             var googleUser = auth2.currentUser.get();
-            
             // Change user's profile information
             changeProfile(googleUser);
+            if (googleUser.hasGrantedScopes("https://mail.google.com/")) {
+              console.log('granted');
+            }
           }
+          else{
+            console.log('signing in');
+            //window.location.replace(google);
+            auth2.signIn()
+              .then(changeProfile);
+          };
+                     
+            //window.location.replace(google);
         });
       });
+
+      /*if(window.location.href != home && window.location.href != google){
+        signedin == true;
+      }
+      if(signedin == false){
+        window.location.replace(google);
+      }
+      else{
+        console.log(window.location.href);
+      }
+      */
+      
+      /*var authCode : string = window.location.href;
+      if(window.location.href != google && window.location.href != home ){
+        window.location.replace(google);
+        console.log(window.location.href);
+        window.location.replace("https://arcane-refuge-67249.herokuapp.com/");
+      }
+      else if(window.location.href != google){
+        window.location.replace(google);
+        console.log('g');
+      }
+
+      */
+      //window.location.replace(window.location.href);
     // Create the object that controls the "Edit Entry" form
     // set up initial UI state
-    $("#editElement").hide();*/
+    $("#editElement").hide();
 });
