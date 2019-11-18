9cf11f1ab86e8b0913d326a269e50d83dead1ab9
AliceLiu97 <liu.alice@cheverus.org>
2019-11-18 01:20:04 -0500

    able user to login, able to upload picture, able camera, but still have issue to store it into gallary.

--- android/app/src/main/java/edu/lehigh/cse216/yut222/phase0/SecondActivity.java
+++ android/app/src/main/java/edu/lehigh/cse216/yut222/phase0/SecondActivity.java
@@ -3,7 +3,11 @@ package edu.lehigh.cse216.yut222.phase0;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.FileUtils;
 import android.os.Bundle;
+import android.util.Base64;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -15,25 +19,43 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+import android.os.Environment;
 
 import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.JSONObject;;
 
 import java.lang.reflect.Type;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import static edu.lehigh.cse216.yut222.phase0.LoginActivity.sharedpreferences;
 
 public class SecondActivity extends AppCompatActivity {
+
+    String image;
+    ImageView imageView;
+    String currentPhotoPath;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,18 +72,54 @@ public class SecondActivity extends AppCompatActivity {
         final EditText etTitle = (EditText) findViewById(R.id.editText);
         final EditText etContent = (EditText) findViewById(R.id.editText4);
 
+        Button bGG = (Button)findViewById(R.id.buttonGoGallery);
+        bGG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create an Intent with action as ACTION_PICK
+                Intent intent=new Intent(Intent.ACTION_PICK);
+                // Sets the type as image/*. This ensures only components of type image are selected
+                intent.setType("image/*");
+                //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+                String[] mimeTypes = {"image/jpg", "image/png"};
+                intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+                // Launching the Intent
+                startActivityForResult(intent,0);
+            }
+        });
+
+        Button bTP = (Button) findViewById(R.id.buttonTakePicture);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        if (!hasCamera()) {
+            bTP.setEnabled(false);
+        }
+        bTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+
+            }
+        });
+
         Button bOk = (Button) findViewById(R.id.buttonOk);
         bOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent();
                 if (!etTitle.getText().toString().equals("")&& !etContent.getText().toString().equals("") ) {
+                    java.io.File file = new java.io.File (image);
+                    //byte[] fileContent = FileUtils.readFileToByteArray(file)); version too new
+                    //String encodedString = java.util.Base64.getEncoder().encodeToString(fileContent);
+                    byte [] fileContent = loadFile(file);
+                    int flags = Base64.NO_WRAP | Base64.URL_SAFE;
+                    String encodedString = Base64.encodeToString(fileContent, flags);
                     i.putExtra("title", etTitle.getText().toString());
                     i.putExtra("content", etContent.getText().toString());
                     setResult(Activity.RESULT_OK, i);
                     String title = etTitle.getText().toString();
                     String content = etContent.getText().toString();
-                    postMessage(title, content, sharedpreferences.getString("prefId","default"));//uid is 7 for now
+                    postMessage(title, content, encodedString, sharedpreferences.getString("prefId","default"));
                 }
                 finish();
 
@@ -79,7 +137,137 @@ public class SecondActivity extends AppCompatActivity {
         });
     }
 
-    private void postMessage(final String t, final String c, final String uid){
+    private boolean hasCamera() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    private static byte[] loadFile(File file){
+        try {
+            java.io.InputStream is = new FileInputStream(file);
+
+            long length = file.length();
+            if (length > Integer.MAX_VALUE) {
+                // File is too large
+            }
+            byte[] bytes = new byte[(int) length];
+
+            int offset = 0;
+            int numRead = 0;
+            while (offset < bytes.length
+                    && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+                offset += numRead;
+            }
+
+            if (offset < bytes.length) {
+                throw new IOException("Could not completely read file " + file.getName());
+            }
+
+            is.close();
+            return bytes;
+        } catch (IOException e) {
+            System.out.println("convertion from file to 64 bits error failed");
+            return new byte[0]; //not certain of this return type
+        }
+    }
+
+    /*
+    private void pickFromGallery(){
+        //Create an Intent with action as ACTION_PICK
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+        //the youtube video only use 0 instead of GALLERY_REQUEST_CODE
+        startActivityForResult(intent,GALLERY_REQUEST_CODE);
+
+    }
+    */
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+            // Result code is RESULT_OK only if the user selects an Image
+            if (resultCode == RESULT_OK && requestCode == 0)
+                if (data == null) {
+                    Toast.makeText(this, "Unable to choose image", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            android.net.Uri imageUri = data.getData();
+            image = getRealPathFromUri(imageUri);
+        }
+
+    }
+
+    private String getRealPathFromUri (android.net.Uri uri){
+        String[] projection = {MediaStore.Images.Media.DATA};
+        androidx.loader.content.CursorLoader loader = new androidx.loader.content.CursorLoader (getApplicationContext(), uri, projection, null, null, null);
+        android.database.Cursor cursor = loader.loadInBackground();
+        int column_idx = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_idx);
+        cursor.close();
+        return result;
+    }
+
+/*
+//take pic without saving it
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+ */
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("CreateImageFile() wrong", "No idea how to fix");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    //t -> title, c -> content, e -> encodedString
+    private void postMessage(final String t, final String c, final String e, final String uid){
         //map is hashMap, m is jsonObject
         //one link to post
         String urlPost = "https://arcane-refuge-67249.herokuapp.com/messages";
@@ -87,7 +275,9 @@ public class SecondActivity extends AppCompatActivity {
         map.put("uid", uid);
         map.put("mTitle", t);
         map.put("mMessage", c);
-
+        map.put("fileData", e);
+        map.put("mLink", "");
+        map.put("mimeType", "image/*");
         map.put("sessionKey", sharedpreferences.getString("prefKey", "default"));
         map.put("uEmail", sharedpreferences.getString("prefEmail", "default"));
         JSONObject m = new JSONObject(map);
@@ -105,7 +295,7 @@ public class SecondActivity extends AppCompatActivity {
                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                             startActivity(intent);
                         }
-                        Log.d("get detail", "mStatus is not ok.");
+                        Log.d("get detail", "mStatus is not ok: " + response.getString("mMessage"));
                     }
                 } catch (final JSONException e) {
                     Log.d("shy221", "Error parsing JSON file: " + e.getMessage());
